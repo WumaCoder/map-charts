9e93bcf8b107968baff117efb41ec087b48cca0d
WumaCoder <1829913225@qq.com>
2021-06-17 21:37:42 +0800

    create code

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <MapCharts></MapCharts>
+  </div>
+</template>
+
+<script>
+import MapCharts from "./components/MapCharts.vue";
+
+export default {
+  name: "App",
+  components: {
+    MapCharts,
+  },
+};
+</script>
